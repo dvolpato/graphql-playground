f562a38c45b6bef1fcaaca7480df391356bd4699
Daniel Pereira Volpato <dvolpato@users.noreply.github.com>
2023-02-28 18:13:58 -0300

    Add backend-graphql-codegen_from-hasura-learn/

---
+++ backend-graphql-codegen_from-hasura-learn/server-codegen.ts
@@ -0,0 +1,15 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const server: CodegenConfig = {
+  schema: "schema.graphql",
+  generates: {
+    "./src/gql/server/resolvers-types.ts": {
+      config: {
+        useIndexSignature: true,
+      },
+      plugins: ["typescript", "typescript-resolvers"],
+    },
+  },
+};
+
+export default server;
